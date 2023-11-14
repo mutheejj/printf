ae558d372126ec2f2e45391a35223f5af943db29
john maina <johnmuthee547@gmail.com>
2023-11-14 07:39:56 -0800

    l

--- _printf.c
+++ _printf.c
@@ -24,23 +24,11 @@ int _printf(const char *format, ...)
 		}
 		else {
 			format++;
-			switch (*format){
-<<<<<<< HEAD
-			case 'r':{
-				char *s = va_arg(args, char*);
-				int length = 0;
-				while (s[length] != '\0')
-				{
-					_putchar(s[length]);
-					length++;
-					i++;
-				}
-=======
+			switch (*format){	
 			case 'r': {
 				int r = va_arg(args, int);
 				_putchar(r);
 				i++;
->>>>>>> 32f0c0add66882f285013fecd9741882d08c3583
 				break;
 				 }
 			case 'c':{
@@ -71,11 +59,6 @@ int _printf(const char *format, ...)
 		}
 		format++;
 	}
-<<<<<<< HEAD
-=======
-	my_printf(format, args);
-	_printf(format, args);
->>>>>>> 32f0c0add66882f285013fecd9741882d08c3583
 	va_end(args);
 	return (i);
 }
